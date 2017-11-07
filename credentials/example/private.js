d68979cfc5587b05c2310f3c755ab9376cf93b24
Artem Shelkov <shelkov1991@gmail.com>
2017-11-07 23:10:29 +0700

    Some initial app copied from lottery.

---
+++ credentials/example/private.js
@@ -0,0 +1,6 @@
+"use strict";
+
+module.exports = {
+    // This data is used during OAuth authentication to Slack. Must be securely stored. Obtained on Slack app dashboard in OAuth tab.
+    client_secret: '<>'
+};
